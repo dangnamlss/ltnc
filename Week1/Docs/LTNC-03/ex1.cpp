2580745003a415b50688bbf8490ff36f35fa65d6
dangnamlss <dangnamlss@gmail.com>
2023-07-17 18:10:02 +0700

    update ex1. ex2 LTNC_03

--- Week1/Docs/LTNC-03/ex1.cpp
+++ Week1/Docs/LTNC-03/ex1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+string newString(string a, char replaceElement)
+{
+    string newStr = replaceElement + a.substr(1);
+    return newStr;
+}
 
 int main()
 {
-    std::cout << "Hello World!";
+    string a, b;
+    cin >> a >> b;
+
+    cout << a.length() << ' ' << b.length() << endl;
+    cout << a + b << endl;
+    cout << newString(a, b[0]) << ' ' << newString(b, a[0]) << endl;
+
     return 0;
 }
